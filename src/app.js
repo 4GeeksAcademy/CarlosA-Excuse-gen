70b7e8a46f530ef075b6142efc5fd3ad2be8bb3f
Carlos Atanes Vences <carlos117g@gmail.com>
2024-05-09 18:03:57 +0000

    Excuse generator terminado
    ;)ç

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function numAleatorio(anArray) {
+    return Math.floor(Math.random() * anArray.length);
+  }
+
+  let excuse = who[numAleatorio(who)] + " ";
+  excuse += action[numAleatorio(action)] + " ";
+  excuse += what[numAleatorio(what)] + " ";
+  excuse += when[numAleatorio(when)];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
